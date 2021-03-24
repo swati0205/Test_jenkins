654b96abcea1b3ac0be1bcb625448267661e48f4
swati0205 <swati0205.in@gmail.com>
2021-03-24 18:00:43 -0400

    Test Change 3

--- src/test/java/Base/Test.java
+++ src/test/java/Base/Test.java
@@ -1,10 +1,16 @@
 package Base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test {
+    public static WebDriver driver;
+
     public static void main(String[] args) {
 
-        System.out.println("Swati Singh");
-
-        System.out.println("This is the test");
+        System.setProperty("webdriver.chrome.driver", "/Users/swatisingh/Documents/DevTools/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("http://www.amazon.ca");
+        driver.manage().window().maximize();
     }
 }
