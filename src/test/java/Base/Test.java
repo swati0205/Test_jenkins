8a63e67e5eecff407800fee99e168109b3cfe35d
swati0205 <swati0205.in@gmail.com>
2021-03-24 18:09:25 -0400

    Test annot added

--- src/test/java/Base/Test.java
+++ src/test/java/Base/Test.java
@@ -6,7 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Test {
     public static WebDriver driver;
 
-    public static void main(String[] args) {
+    @org.testng.annotations.Test
+    public void initialize(String[] args) {
 
         System.setProperty("webdriver.chrome.driver", "/Users/swatisingh/Documents/DevTools/chromedriver");
         driver = new ChromeDriver();
